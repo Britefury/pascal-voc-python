ded559f81001e3b360c4d29adb53330a75349814
Geoffrey French <brittix1023@googlemail.com>
2016-10-31 23:34:35 +0000

    Simplified API. Annotations loaded from XML and cached in HDF file for faster access.

--- voc_utils/voc_utils.py
+++ voc_utils/voc_utils.py
@@ -1,6 +1,7 @@
 import six
 import pandas as pd
-import os
+import os, sys
+import cPickle
 from bs4 import BeautifulSoup
 # from more_itertools import unique_everseen
 import numpy as np
@@ -41,6 +42,11 @@ class PascalVOC (object):
         trainval_image_names = pd.read_csv(trainval_path, delim_whitespace=True, header=None, names=['filename'])
         self.trainval_image_names = set(trainval_image_names['filename'].values)
 
+        self.all_image_names = set([os.path.splitext(filename)[0] for filename in os.listdir(self.img_dir)])
+
+        self.__all_annotations = None
+        self.__all_image_metadata = None
+
     @staticmethod
     def _read_image_list(path):
 
@@ -68,66 +74,13 @@ class PascalVOC (object):
         df = pd.read_csv(filename, delim_whitespace=True, header=None, names=['filename', 'presence'])
         return df
 
-    def annotation_path(self, img_name):
-        """
-        Given an image name, get the annotation file for that image
-
-        Args:
-            img_name (string): string of the image name, relative to
-                the image directory.
-
-        Returns:
-            string: file path to the annotation file
-        """
-        return os.path.join(self.ann_dir, img_name) + '.xml'
-
-    def get_annotation_xml(self, img_filename):
-        """
-        Load annotation file for a given image.
-
-        Args:
-            img_name (string): string of the image name, relative to
-                the image directory.
-
-        Returns:
-            BeautifulSoup structure: the annotation labels loaded as a
-                BeautifulSoup data structure
-        """
-        xml = ""
-        with open(self.annotation_path(img_filename)) as f:
-            xml = f.readlines()
-        xml = ''.join([line.strip('\t') for line in xml])
-        return BeautifulSoup(xml)
-
-    def get_objects_in_images(self, img_names):
-        if isinstance(img_names, six.string_types):
-            img_names = [img_names]
-
-        data = []
-        for img_name in img_names:
-            anno = self.get_annotation_xml(img_name)
-
-            fname = anno.findChild('filename').contents[0]
-            objs = anno.findAll('object')
-            for obj in objs:
-                obj_names = obj.findChildren('name')
-                cat = obj_names[0].contents[0]
-                bbox = obj.findChildren('bndbox')[0]
-                xmin = int(bbox.findChildren('xmin')[0].contents[0])
-                ymin = int(bbox.findChildren('ymin')[0].contents[0])
-                xmax = int(bbox.findChildren('xmax')[0].contents[0])
-                ymax = int(bbox.findChildren('ymax')[0].contents[0])
-                data.append([fname, cat, xmin, ymin, xmax, ymax])
-
-        return pd.DataFrame(
-            data, columns=['image_filename', 'category', 'xmin', 'ymin', 'xmax', 'ymax'])
-
-    def get_objects(self, image_names=None, category=None, dataset=None):
+    def _get_image_names(self, image_names, category, dataset):
         datasets_by_name = {'train': self.train_image_names,
                             'val': self.val_image_names,
                             'trainval': self.trainval_image_names}
         if image_names is not None:
-            pass
+            if isinstance(image_names, six.string_types):
+                image_names = [image_names]
         else:
             if dataset is None and category is None:
                 image_names = self.trainval_image_names
@@ -140,12 +93,132 @@ class PascalVOC (object):
                     imgs = self.imgs_from_category(category, dataset)
                     imgs = imgs[imgs['presence'] == self.PRESENCE_TRUE]
                     image_names = list(imgs['filename'].values)
+        return image_names
 
-        objs = self.get_objects_in_images(image_names)
-        if category is not None:
-            objs = objs[objs['category'] == category]
-        return objs
 
+    def __annotation_path(self, img_name):
+        """
+        Given an image name, get the annotation file for that image
+
+        Args:
+            img_name (string): string of the image name, relative to
+                the image directory.
+
+        Returns:
+            string: file path to the annotation file
+        """
+        img_name = os.path.splitext(img_name)[0]
+        return os.path.join(self.ann_dir, img_name) + '.xml'
+
+    def __get_annotation_xml(self, img_filename):
+        """
+        Load annotation file for a given image.
+
+        Args:
+            img_name (string): string of the image name, relative to
+                the image directory.
+
+        Returns:
+            BeautifulSoup structure: the annotation labels loaded as a
+                BeautifulSoup data structure
+        """
+        xml = ""
+        with open(self.__annotation_path(img_filename)) as f:
+            xml = f.readlines()
+        xml = ''.join([line.strip('\t') for line in xml])
+        return BeautifulSoup(xml)
+
+
+    def __read_all_annotations(self):
+        if self.__all_annotations is None or self.__all_image_metadata is None:
+            # Not loaded; see if the dataframe files exists
+            annotations_df_path = os.path.join(self.ann_dir, 'all_annotations.h5')
+            img_meta_df_path = os.path.join(self.ann_dir, 'all_metadata.h5')
+            if not os.path.exists(annotations_df_path) or not os.path.exists(img_meta_df_path):
+                # No; load the XML annotations and cache
+                anno_data = []
+                img_meta = []
+
+                for img_i, img_name in enumerate(self.all_image_names):
+                    anno = self.__get_annotation_xml(img_name)
+
+                    train = img_name in self.train_image_names
+                    val = img_name in self.val_image_names
+                    trainval = img_name in self.trainval_image_names
+
+                    fname = str(anno.findChild('filename').contents[0])
+                    objs = anno.findAll('object')
+                    for obj in objs:
+                        obj_names = obj.findChildren('name')
+                        cat = str(obj_names[0].contents[0])
+                        bbox = obj.findChildren('bndbox')[0]
+                        xmin = float(bbox.findChildren('xmin')[0].contents[0])
+                        ymin = float(bbox.findChildren('ymin')[0].contents[0])
+                        xmax = float(bbox.findChildren('xmax')[0].contents[0])
+                        ymax = float(bbox.findChildren('ymax')[0].contents[0])
+                        occ_xml = obj.findChildren('occluded')
+                        trunc_xml = obj.findChildren('truncated')
+                        diff_xml = obj.findChildren('difficult')
+                        occluded = truncated = difficult = False
+                        if len(occ_xml) > 0:
+                            occluded = bool(int(occ_xml[0].contents[0]))
+                        if len(trunc_xml) > 0:
+                            truncated = bool(int(trunc_xml[0].contents[0]))
+                        if len(diff_xml) > 0:
+                            difficult = bool(int(diff_xml[0].contents[0]))
+                        anno_data.append([fname, cat, xmin, ymin, xmax, ymax, occluded, truncated, difficult, train,
+                                          val, trainval])
+
+                    size = anno.findChild('size')
+                    width = int(size.findChildren('width')[0].contents[0])
+                    height = int(size.findChildren('height')[0].contents[0])
+                    depth = int(size.findChildren('depth')[0].contents[0])
+                    img_meta.append([fname, width, height, depth, train, val, trainval])
+
+                    sys.stdout.write('\r{}'.format(img_i))
+
+                anno_df = pd.DataFrame(
+                    anno_data, columns=['image_filename', 'category', 'xmin', 'ymin', 'xmax', 'ymax', 'occluded',
+                                        'truncated', 'difficult', 'train', 'val', 'trainval'])
+
+                img_meta_df = pd.DataFrame(
+                    img_meta, columns=['image_filename', 'width', 'height', 'depth', 'train', 'val',
+                                       'trainval'])
+
+                anno_df.to_hdf(annotations_df_path, 'annotations')
+                img_meta_df.to_hdf(img_meta_df_path, 'img_metadata')
+
+                self.__all_annotations = anno_df
+                self.__all_image_metadata = img_meta_df
+            else:
+                self.__all_annotations = pd.read_hdf(annotations_df_path, 'annotations')
+                self.__all_image_metadata = pd.read_hdf(img_meta_df_path, 'img_metadata')
+
+    @property
+    def all_annotations(self):
+        self.__read_all_annotations()
+        return self.__all_annotations
+
+    @property
+    def all_image_metadata(self):
+        self.__read_all_annotations()
+        return self.__all_image_metadata
+
+
+    def get_annotations(self, image_names=None, category=None, dataset=None):
+        df = self.all_annotations
+        df = self.__dataset_filter(df, dataset)
+        df = self.__category_filter(df, category)
+        df = self.__image_names_filter(df, image_names)
+        return df
+
+
+    def get_image_metadata(self, image_names=None, category=None, dataset=None):
+        df = self.all_image_metadata
+        df = self.__image_names_filter(df, image_names)
+        if image_names is not None:
+            df = df[df.apply(lambda x: x['image_filename'] in image_names, axis=1)]
+        return df
 
     def load_img(self, img_filename):
         """
@@ -159,13 +232,43 @@ class PascalVOC (object):
         Returns:
             np array of float32: an image as a numpy array of float32
         """
-        img_filename = os.path.join(self.img_dir, img_filename + '.jpg')
-        img = skimage.img_as_float(io.imread(
-            img_filename)).astype(np.float32)
+        if os.path.splitext(img_filename)[1] == '':
+            img_filename = img_filename + '.jpg'
+        img_filename = os.path.join(self.img_dir, img_filename)
+        img = skimage.img_as_float(io.imread(img_filename)).astype(np.float32)
         if img.ndim == 2:
             img = img[:, :, np.newaxis]
         elif img.shape[2] == 4:
             img = img[:, :, :3]
         return img
+
+
+    def __image_names_filter(self, df, image_names=None):
+        if image_names is None:
+            return df
+        else:
+            return df[df.apply(lambda x: x['image_filename'] in image_names, axis=1)]
+
+
+    def __category_filter(self, df, category=None):
+        if category is None:
+            return df
+        elif category in self.CATEGORY_NAMES:
+            return df[df['category'] == category]
+        else:
+            raise ValueError('Invalid category \'{}\''.format(category))
+
+
+    def __dataset_filter(self, df, dataset=None):
+        if dataset is None:
+            return df
+        elif dataset == 'train':
+            return df[df['train'] == True]
+        elif dataset == 'val':
+            return df[df['val'] == True]
+        elif dataset == 'trainval':
+            return df[df['trainval'] == True]
+        else:
+            raise ValueError('Invalid dataset \'{}\'; valid values are train, val or trainval'.format(dataset))
 
 
